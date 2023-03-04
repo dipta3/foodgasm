b734567de33c28d7b0314fa5d43d3fe9b965096a
Dipta Biswas <diptabiswas79@gmail.com>
2023-03-04 12:05:36 +0600

    file setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './Router/Routes/Routes';
 
 function App() {
   return (
-    <div>
-
+    <div className='max-w-screen-xl mx-auto'>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
