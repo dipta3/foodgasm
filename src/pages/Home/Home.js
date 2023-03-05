804bfd69ebbdd7a76d2fa7507d860ca6762666e6
Dipta Biswas <diptabiswas79@gmail.com>
2023-03-05 11:38:27 +0600

    banner added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Typewriter from 'typewriter-effect';
 
 const Home = () => {
+
     return (
-        <div>
-            <h2>This is home</h2>
+        <div className="hero min-h-screen bg-[url('/src/assets/banner.jpg')]">
+            <div className="hero-overlay bg-opacity-40"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-md">
+                    <h1 className="mb-5 font-serif text-7xl font-bold text-red-500">
+                        <Typewriter
+                            options={{
+                                strings: ['Textured?', 'Juicy?', 'Tender?'],
+                                autoStart: true,
+                                loop: true,
+                                cursor: '',
+
+                            }}
+                        />
+                    </h1>
+                    <p className="mb-5 text-xl font-bold text-white">Every Food Reviewed Here</p>
+                    <button className="btn btn-primary"><Link>Leave A Review</Link></button>
+                </div>
+            </div>
         </div>
     );
 };
