22894f71a6ae50018c4cbae0cc7aae55c746f225
Dipta Biswas <diptabiswas79@gmail.com>
2023-03-09 15:47:36 +0600

    profile Name photo url added

--- src/Contexts/AuthProvider/AuthProvider.js
+++ src/Contexts/AuthProvider/AuthProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth'
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth'
 import app from '../../firebase/firebase.config';
 export const AuthContext = createContext();
 
@@ -18,7 +18,7 @@ const AuthProvider = ({ children }) => {
         return createUserWithEmailAndPassword(auth, email, password)
     }
     const updateUserProfile = (profile) => {
-        return updateUserProfile(auth.currentUser, profile)
+        return updateProfile(auth.currentUser, profile)
     }
     const login = (email, password) => {
         setLoading(true)
